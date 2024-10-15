9f2e31501eac65723628d3933a8a74fde1b56e66
Dom <86100131+dj4815@users.noreply.github.com>
2024-10-15 14:46:52 +0200

    pixelschieben 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import CardContent from "./CardContent";
 export default function Card() {
   return (
     <>
-      <div className="container-2xl bg-blue-500 p-12 sm:p-32 m-12 l:mx-96">
+      <div className="container-2xl bg-blue-500 p-12 sm:p-32 m-12 l:m-32 l:mx-96">
         <CardContent />
       </div>
     </>

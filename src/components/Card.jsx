ad84e90c9b0d4cc6d11a37a44e2a3c250752f3a9
Dom <86100131+dj4815@users.noreply.github.com>
2024-10-23 10:21:25 +0200

    more lighthouse appeasement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import CardContent from "./CardContent";
 export default function Card() {
   return (
     <>
-      <div className="container-2xl bg-blue-500 p-12 sm:p-32 mx-12 my-24 lg:mx-48 xl:mx-96">
+      <div className="container-2xl bg-blue-600 p-12 sm:p-32 mx-12 my-24 lg:mx-48 xl:mx-96">
         <CardContent />
       </div>
     </>

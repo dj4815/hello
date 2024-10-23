ba5a03ac8089a9442df13cf3994f73494f247d16
Dom <86100131+dj4815@users.noreply.github.com>
2024-10-23 10:26:07 +0200

    rounding

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -1,7 +1,7 @@
 export default function Button2() {
   return (
     <button
-      className="bg-black border-2 border-black hover:bg-white transition duration-500 text-white hover:text-black text-xl font-medium px-4 py-2 rounded-md mt-8 "
+      className="bg-black border-2 border-black hover:bg-white transition duration-500 text-white hover:text-black text-xl font-medium px-4 py-2 rounded-xl mt-8 "
       type="button"
     >
       Button Two

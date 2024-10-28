6e14a49c8face5caba95aef99e3c8cf81a45484b
Dom <86100131+dj4815@users.noreply.github.com>
2024-10-28 10:33:14 +0100

    add content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,14 +8,8 @@ export default function Main() {
     <>
       <Section
         image="./assets/star-trails.jpg"
-        heading="I am Section One."
-        paragraph="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, labore
-        odio tempora dolorem voluptates reprehenderit, molestias doloribus
-        voluptatibus provident illum eaque? Possimus atque ea quas cupiditate
-        assumenda! Placeat, beatae dolor. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Repellat libero rerum voluptatum dicta, harum saepe
-        dolorem cupiditate officia nesciunt optio sunt assumenda voluptatibus
-        eum? Inventore, maxime! Delectus odio placeat fuga."
+        heading="Hello there!"
+        paragraph="Welcome to my website. As you can see, this is a work in progress. There is not much content yet, as I am still working on the base structure and components of the website. The finished product should have a homepage (you're viewing it right now) and a projects section, where I am going to show off what I have. I also want to implement a language switcher German <> English, as well as a dark mode. So for now, everything is going to be super bright and in English only, sorry about that! Also, the page is going to be extra messy on desktop, as I am doing mobile first."
         buttonText="Primary Button"
         buttonVariant="primary"
       />

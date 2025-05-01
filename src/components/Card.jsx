f7bcc0e566639bb695a66e3954a063067046dd75
Dom <86100131+dj4815@users.noreply.github.com>
2025-05-02 00:03:22 +0200

    message

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,8 @@ export default function Card({
 }) {
   return (
     <div
-      className={`${bgColor} container-2xl px-8 py-12 sm:p-32 mx-8 my-16 lg:mx-48 xl:mx-96 rounded-xl`}
+      // check container documentation
+      className={`${bgColor} max-w-screen-xl container-xl lg:mx-auto mx-4 my-16 px-8 py-12 sm:p-32 rounded-xl`}
     >
       <Heading2 text={heading} style={headingStyle} />
       <Paragraph text={paragraph} paragraphColor={paragraphColor} />
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,10 +5,10 @@ export default function Image({ image, heading1Style, heading1 }) {
     <div className="relative w-full h-[500px] overflow-hidden mb-4">
       <img
         src={image}
-        className="w-full h-full object-cover object-center lg:w-[200%] lg:object-left"
+        className="w-full h-full object-cover object-center lg:w-[200%] lg:object-left rounded-xl"
         alt="background"
       />
-          <Heading1 text={heading1} style={heading1Style} />
+      <Heading1 text={heading1} style={heading1Style} />
     </div>
   );
 }

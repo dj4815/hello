3ca9fce43600d7fe21153b474f19efb8b0f0c97a
Dom <86100131+dj4815@users.noreply.github.com>
2025-05-16 20:08:30 +0200

    changes

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,6 @@
 import Button from "./Button";
 import Heading2 from "./Heading2";
 import Image from "./Image";
-import Paragraph from "./Paragraph";
 
 export default function Section({
   heading2,
@@ -21,7 +20,7 @@ export default function Section({
         noTopPadding ? "pt-0 xl:pt-16" : "pt-16"
       }`}
     >
-      <div className="max-w-screen-xl mx-auto grid grid-cols-1 xl:grid-cols-2 gap-4">
+      <div className="max-w-screen-xl mx-auto grid grid-cols-1 xl:grid-cols-2 lg:gap-4">
         <div className="flex justify-center lg:justify-end">
           <Image
             image={image}
@@ -29,7 +28,7 @@ export default function Section({
             heading1Style={heading1Style}
           />
         </div>
-        <div className="bg-white lg:ml-4 p-4 lg:p-8 flex flex-col items-start justify-center text-left">
+        <div className="bg-white mt-0 lg:ml-4 pt-16 pb-16 pl-4 pr-4 lg:p-8 flex flex-col items-start justify-center text-left">
           <Heading2 text={heading2} style={heading2Style} />
           <div className="space-y-4 text-gray-700">{children}</div>
           {content}

1971eb81ef9b4e52d63a0579ba864657068a5659
Dom <86100131+dj4815@users.noreply.github.com>
2025-05-21 12:38:37 +0200

    more projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,7 @@ export default function Projects() {
         heading1="Projects"
         heading1Style="primaryBg"
       ></HeroImage>
+
       <Section
         image="./assets/selection-sort.jpg"
         buttonText={["View Code"]}
@@ -24,12 +25,41 @@ export default function Projects() {
           algorithm in C, applied to a randomly generated array of 50,000 unique
           integers. The algorithm repeatedly selects the smallest element from
           the unsorted portion of the array and swaps it with the element at the
-          current position.</p><p> Although not the most efficient for large datasets
-          due to its O(n²) time complexity, this implementation demonstrates a
-          fundamental sorting technique often used for educational purposes. The
-          project includes additional arrays sorted in ascending, descending,
-          and random order to provide a foundation for comparing the performance
-          of different sorting algorithms.
+          current position.
+        </p>
+        <p>
+          Although not the most efficient for large datasets due to its O(n²)
+          time complexity, this implementation demonstrates a fundamental
+          sorting technique often used for educational purposes. The project
+          includes additional arrays sorted in ascending, descending, and random
+          order to provide a foundation for comparing the performance of
+          different sorting algorithms.
+        </p>
+      </Section>
+
+      <Section
+        image="./assets/indigo-and-orange.jpg"
+        buttonText={["View Code"]}
+        buttonVariants={["blackPrimary"]}
+      >
+        <h2 className="text-4xl font-bold mb-4">
+          Website: Indigo and Orange{" "}
+          <span className="bg-black text-white rounded-full px-4">
+            React/Tailwind
+          </span>
+        </h2>
+        <p>
+          Indigo and Orange was my first real dive into web design using React
+          and Tailwind CSS. The project started as an experiment to explore
+          modern front-end tools and turned into a hands-on way to learn
+          component-based development, responsive design, and styling with
+          utility-first CSS.
+        </p>
+        <p>
+          While it's simple, it marks the beginning of my journey into building
+          user interfaces from scratch and taught me a lot about structuring
+          code, working with state, and making things look good on different
+          screen sizes.
         </p>
       </Section>
     </>

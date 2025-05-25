1c31e251123c5887ea281e74fef2713c053898f9
Dom <86100131+dj4815@users.noreply.github.com>
2025-05-25 13:46:21 +0200

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="container-2xl text-center bg-white p-12 border-t-2">
-      <p>Made with React and Tailwind CSS</p>
-      <p>
-        <a
-          className="underline underline-offset-4 decoration-blue-700 decoration-2"
-          href="https://github.com/dj4815"
-        >
-          GitHub
-        </a>{" "}
-        |{" "}
-        <a
-          className="underline underline-offset-4 decoration-blue-700 decoration-2"
-          href="www.linkedin.com/in/dj4815"
-        >
-          LinkedIn
-        </a>
-      </p>
+    <div className="container-2xl text-center bg-white p-8 border-t-2">
+      <p>Made in 2024 with React and Tailwind CSS</p>
     </div>
   );
 }
